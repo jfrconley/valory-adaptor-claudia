1e2a465a1481160b0e9c86627205fa426975375e
John Conley <john.conley@modopayments.com>
2018-08-22 14:50:02 -0500

    feat(adaptor): added claudia context attachment

--- src/adaptor-claudia.ts
+++ src/adaptor-claudia.ts
@@ -2,7 +2,26 @@ import {ApiResponse, ApiServer, HttpMethod, ValoryMetadata, ApiRequest} from "va
 
 const ApiBuilder = require("claudia-api-builder");
 
+export interface ClaudiaContext {
+	method: string;
+	path: string;
+	stage: string;
+	sourceIp: string;
+	accountId?: string;
+	user?: string;
+	userAgent: string;
+	userArn?: string;
+	caller?: string;
+	apiKey: string;
+    authorizerPrincipalId?: string;
+    cognitoAuthenticationProvider?: string
+    cognitoAuthenticationType?: string;
+    cognitoIdentityId?: string;
+    cognitoIdentityPoolId?: string
+}
+
 export class ClaudiaAdaptor implements ApiServer {
+	public static ClaudiaContextKey = ApiRequest.createKey<ClaudiaContext>();
 	public allowDocSite: boolean = true;
 	public locallyRunnable: boolean = false;
 	private instance = new ApiBuilder();
@@ -35,6 +54,7 @@ export class ClaudiaAdaptor implements ApiServer {
 				path: req.pathParams,
 				route: `${path}:${stringMethod}`,
 			});
+			tranReq.putAttachment(ClaudiaAdaptor.ClaudiaContextKey, req.context);
 
 			const handlerResp = await handler(tranReq);
 			responseObj.response = handlerResp.body;
